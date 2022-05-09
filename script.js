e44c8e23cf5b4db0c3a064703de629ee6b7a595b
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2022-05-09 13:27:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-//Main js script file
+//Check if Safari or Chrome:
+
 
 
 //Store hooks and video sizes:
@@ -13,16 +14,21 @@ var vidHeight = 0;
 var xStart = 0;
 var yStart = 0;
 
+
+
+const imageSize = 375;
+//const captureSize={w:100,h:50};//pxels
 const captureSize = {
   w: 150,
   h: 60
 }; //pxels
-
+//Match prob. threshold for object detection:
+var classProbThreshold = 0.4; //40%
 
 
 //Creating canvas for OCR:
 const c = document.createElement('canvas');
-
+//const c = document.getElementById('canvas');
 c.width = captureSize.w;
 c.height = captureSize.h;
 var click_pos = {
@@ -47,8 +53,6 @@ var context = canvas.getContext('2d');
 
 
 
-
-//Called on frame render event:
 function renderFrame() {
   
   // re-register callback
@@ -78,119 +82,22 @@ function renderFrame() {
 }
 
 
-
-
-
-
-
-//Detecting numbers in the pointed rectangle:
-async function detectTFMOBILE(imgToPredict) {
-
-  //Get next video frame:
-  //Perform OCR:
-  if (Analyzef) 
-  {
-    c.getContext('2d').drawImage(canvas, click_pos.x, click_pos.y,
-      captureSize.w, captureSize.h, 0, 0, captureSize.w, captureSize.h);
-    
-    let tempMark = MarkAreaSimple(mouse_pos.x - captureSize.w / 2, mouse_pos.y - captureSize.h / 2, captureSize.w, captureSize.h);
-
-    //Use tessaract to recognize the numbers:
-    let res = await Recognize(c);
-
-    tempMark.remove();
-
-    MarkArea(mouse_pos.x - captureSize.w / 2, mouse_pos.y - captureSize.h / 2, captureSize.w, captureSize.h, res);
-    Analyzef = false;
-
-    //We can use the number to dial using whatsapp:
-    /* if (res.length >= 10)
-      window.location.href = 'sms:' + res.replaceAll('-', '');
-    */
+//Streching image on canvas:
+function calculateSize(srcSize, dstSize) {
+  var srcRatio = srcSize.width / srcSize.height;
+  var dstRatio = dstSize.width / dstSize.height;
+  if (dstRatio > srcRatio) {
+    return {
+      width: dstSize.height * srcRatio,
+      height: dstSize.height
+    };
+  } else {
+    return {
+      width: dstSize.width,
+      height: dstSize.width / srcRatio
+    };
   }
 }
-
-
-
-
-// Check if webcam access is supported.
-function getUserMediaSupported() {
-  return !!(navigator.mediaDevices &&
-    navigator.mediaDevices.getUserMedia);
-}
-
-// If webcam supported, add event listener to activation button:
-if (getUserMediaSupported()) {
-  enableWebcamButton.addEventListener('click', enableCam);
-} else {
-  console.warn('getUserMedia() is not supported by your browser');
-}
-
-
-
-
-// Enable the live webcam view and start detection.
-function enableCam(event) {
-  
-  //Enable click event:
-  canvas.addEventListener("mousedown", Capture, false);
-  canvas.addEventListener('mousedown', function(e) {
-    getCursorPosition(canvas, e)
-  });
-
-  // Hide the button once clicked.
-  enableWebcamButton.classList.add('removed');
-
-  // getUsermedia parameters to force video but not audio.
-  const constraints = {
-    video: true
-  };
-
-  // Stream video to video element
-  navigator.mediaDevices.getUserMedia({
-    video: {
-      facingMode: "environment"
-    },
-  }).then(stream => {
-    let $video = document.querySelector('video');
-    
-    $video.srcObject = stream;
-    
-    $video.onloadedmetadata = () => {
-      vidWidth = $video.videoHeight;
-      vidHeight = $video.videoWidth;
-      
-      //The start position of the video (from top left corner of the viewport)
-      xStart = Math.floor((vw - vidWidth) / 2);
-      yStart = (Math.floor((vh - vidHeight) / 2) >= 0) ? (Math.floor((vh - vidHeight) / 2)) : 0;
-      $video.play();
-      
-      renderFrame(); 
-    }
-  });
-}
-
-
-//Call load function
-Init(); //async load tessaract model
-let model = true;
-
-
-
-var children = [];
-//Perform detection of numbers:
-function predictWebcamTF() {
-  
-  // Now let's start classifying a frame in the stream.
-  detectTFMOBILE(video).then(function() {
-    // Call this function again to keep predicting when the browser is ready.
-    window.requestAnimationFrame(predictWebcamTF);
-    
-  });
-}
-
-
-
 
 
 //Capture video on canvas the image:
@@ -221,6 +128,33 @@ function Capture(e) {
     y: (xy.y - captureSize.h / 2)
   };
   Analyzef = true;
+}
+
+
+
+//Image detects object that matches the preset:
+async function detectTFMOBILE(imgToPredict) {
+
+  //Get next video frame:
+  //Perform OCR:
+  if (Analyzef) 
+  {
+    c.getContext('2d').drawImage(canvas, click_pos.x, click_pos.y,
+      captureSize.w, captureSize.h, 0, 0, captureSize.w, captureSize.h);
+    let tempMark = MarkAreaSimple(mouse_pos.x - captureSize.w / 2, mouse_pos.y - captureSize.h / 2, captureSize.w, captureSize.h);
+
+    let res = await Recognize(c);
+
+    tempMark.remove();
+
+    MarkArea(mouse_pos.x - captureSize.w / 2, mouse_pos.y - captureSize.h / 2, captureSize.w, captureSize.h, res);
+    Analyzef = false;
+
+    //We can use the number to dial using whatsapp:
+    /* if (res.length >= 10)
+      window.location.href = 'sms:' + res.replaceAll('-', '');
+    */
+  }
 }
 
 
@@ -269,22 +203,83 @@ function MarkAreaSimple(minX, minY, width_, height_) {
 
 
 
-//Streching image on canvas:
-function calculateSize(srcSize, dstSize) {
-  var srcRatio = srcSize.width / srcSize.height;
-  var dstRatio = dstSize.width / dstSize.height;
-  if (dstRatio > srcRatio) {
-    return {
-      width: dstSize.height * srcRatio,
-      height: dstSize.height
-    };
-  } else {
-    return {
-      width: dstSize.width,
-      height: dstSize.width / srcRatio
-    };
-  }
+// Check if webcam access is supported.
+function getUserMediaSupported() {
+  return !!(navigator.mediaDevices &&
+    navigator.mediaDevices.getUserMedia);
 }
+
+// If webcam supported, add event listener to activation button:
+if (getUserMediaSupported()) {
+  enableWebcamButton.addEventListener('click', enableCam);
+} else {
+  console.warn('getUserMedia() is not supported by your browser');
+}
+
+
+
+
+// Enable the live webcam view and start detection.
+function enableCam(event) {
+  
+  //Enable click event:
+  canvas.addEventListener("mousedown", Capture, false);
+  canvas.addEventListener('mousedown', function(e) {
+    getCursorPosition(canvas, e)
+  });
+
+  // Hide the button once clicked.
+  enableWebcamButton.classList.add('removed');
+
+  // getUsermedia parameters to force video but not audio.
+  const constraints = {
+    video: true
+  };
+
+  // Stream video from VAR (for safari also)
+  navigator.mediaDevices.getUserMedia({
+    video: {
+      facingMode: "environment"
+    },
+  }).then(stream => {
+    let $video = document.querySelector('video');
+    $video.srcObject = stream;
+    $video.onloadedmetadata = () => {
+      vidWidth = $video.videoHeight;
+      vidHeight = $video.videoWidth;
+      
+      //The start position of the video (from top left corner of the viewport)
+      xStart = Math.floor((vw - vidWidth) / 2);
+      yStart = (Math.floor((vh - vidHeight) / 2) >= 0) ? (Math.floor((vh - vidHeight) / 2)) : 0;
+      $video.play();
+      //Attach detection model to loaded data event:
+      $video.addEventListener('loadeddata', predictWebcamTF);
+
+      renderFrame(); 
+    }
+  });
+}
+
+
+//Call load function
+Init(); //async load tessaract model
+let model = true;
+
+
+
+var children = [];
+//Perform prediction based on webcam using Layer model model:
+function predictWebcamTF() {
+  
+  // Now let's start classifying a frame in the stream.
+  detectTFMOBILE(video).then(function() {
+    // Call this function again to keep predicting when the browser is ready.
+    window.requestAnimationFrame(predictWebcamTF);
+    
+  });
+}
+
+
 
 //Get click on canvas:
 function getCursorPosition(canvas, event) {
