69305f6b02e2ac17bde388849451a2850a769a43
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2022-05-09 12:57:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -217,9 +217,9 @@ if (getUserMediaSupported()) {
 // Enable the live webcam view and start detection.
 function enableCam(event) {
   // Only continue if model has finished loading.
-  if (!model) {
+  /*if (!model) {
     return;
-  }
+  }*/
   //Enable click event:
   canvas.addEventListener("mousedown", Capture, false);
   canvas.addEventListener('mousedown', function(e) {
